185c58625b98551bbe3fd5b249ede539c365f298
Zograb <zograb.matveev@mail.ru>
2020-12-12 10:49:44 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Car from './Car/Car'
 
-function App() {
+class App extends Component {
+   
+  state = {
+    cars : [
+      {name: 'Ford' ,year: 2018},
+      {name: 'Audi' ,year: 2017},
+      {name: 'Mazda' ,year: 2010}
+    ],
+    pageTitle : 'React components',
+    showCars: false
+  }
+  toggleCarsHandler = () => {
+     this.setState({
+      showCars: !this.state.showCars
+      });
+  }
+  onChangeName (name,index) {
+   const car = this.state.cars[index]
+   car.name = name;
+   const cars = [...this.state.cars]
+   cars[index] = car
+   this.setState({cars})
+  }
+  deleteHandler (index) {
+    const cars = this.state.cars.concat();
+    cars.splice(index, 1);
+    this.setState({cars})
+  }
+ 
+  render () {
+    let cars = null
+
+  if (this.state.showCars){
+    cars = this.state.cars.map((car,index) => {
+      return (
+        <Car 
+        key={index}
+        name={car.name}
+        year={car.year}
+        onDelete={this.deleteHandler.bind(this,index)}
+        onChangeName={event => this.onChangeName(event.target.value,index)}
+        />
+      )
+    }) 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>{this.state.pageTitle}</h1>
+    <button onClick={this.toggleCarsHandler}>Toggle Cars</button>
+      <div style={{
+        width: 400,
+        paddingTop: '20px',
+        margin: 'auto'
+      }}>
+    { cars }
+      </div>
+
     </div>
-  );
+   );
+  }
 }
 
-export default App;
+
+
+export default App
