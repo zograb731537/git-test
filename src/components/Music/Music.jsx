7769f92352d4a106ee84a869556c8fbb6f5ed883
zograb731437 <zograb.matveev@mail.ru>
2021-02-02 12:04:38 +0400

    я смог завершить music

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -1,11 +1,66 @@
 import  React from 'react';
+import {NavLink} from 'react-router-dom';
 import s from './Music.module.css';
 
-const Music = (props) => {
+const Singer = (props) => {
+    
+   let path = '/music/' + props.id;
+
+    return(
+        <div>
+              <div className={s.music + " " + s.active} >
+               <NavLink to={path}>{props.name}</NavLink>
+              </div>
+        </div>
+    )
+}
+
+const Songs = (props) => {
+
     return (
-        <div className={s.music}>           
-            <img src="https://is.gd/bTnGck" alt=""/>
-            <input type="text"/>
+        <div>
+         <div className={s.song}>{props.song}</div>
+        </div>
+    )
+}
+
+const Music = (props) => {
+
+    let songs = [
+        {id:"1",song:<a href="https://youtu.be/s3wNuru4U0I?t=2">We are the world</a>},
+        {id:"2",song:<a href="https://youtu.be/6d5ST3tbPIU">Let it be</a>},
+        {id:"3",song:<a href="https://youtu.be/1bGOgY1CmiU">I just called to say I love you</a>},
+    ]
+
+    let Song = songs.map( s => <Songs song={s.song} id={s.id} />);
+
+    let singers = [
+        {id:'1',singer:'Michael Jackson'},
+        {id:'2',singer:'Paul McCartney'},
+        {id:'3',singer:'Stevie Wonder'}
+    ]
+
+     let Sing = singers.map( s => <Singer name={s.singer} id={s.id}/>);   
+
+    return (
+        <div className={s.music + ' ' + s.active}> 
+          <div className={s.musicImg}>
+            <div>          
+            <img src="https://is.gd/Ddj4Rg" />
+            </div>          
+            <div>          
+            <img src="https://is.gd/wHzrfB" />
+            </div>
+            <div>          
+            <img src="https://is.gd/CMl1Oz" />
+            </div> 
+            </div>        
+            <div className={s.musicItem}>
+                {Sing}
+            </div>
+            <div className={s.song}>
+                {Song}
+            </div>
         </div>
     )
  }
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,12 +1,46 @@
 import  React from 'react';
-import s from './News.module.css'
+import s from './News.module.css';
+import {NavLink} from 'react-router-dom';
+
+const MyNews = (props) => {
+
+    let path = "/news/" + props.id
+  return (
+    <div className={s.news}>
+     <NavLink to={path}>{props.name}</NavLink>
+    </div>
+  )
+}
+
+const Greeting = (props) => {
+    return (
+        <div className={s.news}>
+         {props.greeting}
+        </div>
+    )
+}
 
 const News = (props) => {
-   return (
+
+    return (
        <div className={s.news}>
-           <input type="text"/>
-           <img src="https://is.gd/NyTkUO" alt=""/>
+          <div className={s.country}>
+           <MyNews name='RUSSIA' id='1'/>
+           <MyNews name='EUROPE' id='2'/>
+           <MyNews name='AMERIKA' id='3'/>
+           </div>
+           <div className={s.greeting}>
+             <Greeting greeting='ПРИВЕТ'/>  
+             <Greeting greeting='BOJOUR'/>  
+             <Greeting greeting='HELLO'/>  
+           </div>       
+           <div>
+            <img src="https://is.gd/NyTkUO" alt=""/>
+            </div> 
+           
+         
        </div>
+       
    )
 }
 export default News;
