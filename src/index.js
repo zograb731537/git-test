ecf6cb9690e552176b5b21ff3770c6e12657c4e5
zograb731437 <zograb.matveev@mail.ru>
2021-03-07 13:09:27 +0400

    kisat gorc

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
-import { rerenderEntireTree } from './render';
-import state from './redux/state';
-
+import state, { subscribe } from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { addMessage, addPost, addSinger, updateNewMessage, updateNewText ,updateNewSingerName} from "./redux/state";
+import { BrowserRouter } from 'react-router-dom';
 
   
+ let rerenderEntireTree = () => {
+  ReactDOM.render(
+    <BrowserRouter>
+      <React.StrictMode>
+       <App state={state} addPost={addPost} 
+       addMessage={addMessage} addSinger={addSinger}
+       updateNewText={updateNewText} updateNewMessage={updateNewMessage}
+       updateNewSingerName={updateNewSingerName}/>
+      </React.StrictMode>
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
+}
 
 rerenderEntireTree(state);
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,82 +1,76 @@
-import { rerenderEntireTree } from "../render";
-
-
-
-let state = {
-  profilePage : {
-    posts: [
-      {id: 1,message: " Hello , how are you!",count:15},
-      {id: 2,message: " It's my first post!",count:18},
-      {id: 3,message: " I want to be a programmer!",count:20},
-      {id: 4,message: "I feel I can do it!",count:10},
-    ],
-    newPostText : "It-kamasutra",
-  }, 
-  dialogsPage  : {
-    dialogs: [
-      {id: 1,name: "Eduard"},
-      {id: 2,name: "Armen"},
-      {id: 3,name: "Vardan"},
-      {id: 4,name: "Karen"},
-      {id: 5,name: "Sargis"},
-    ],
-    
-    messages: [
-      {id: 1,message: "Hello!"},
-      {id: 2,message: "How are you?"},
-      {id: 3,message: "What is your name?"},
-    ],
-    newPostMessage :"",
+let store = {
+   state : {
+    profilePage : {
+      posts: [
+        {id: 1,message: " Hello , how are you!",count:15},
+        {id: 2,message: " It's my first post!",count:18},
+        {id: 3,message: " I want to be a programmer!",count:20},
+        {id: 4,message: "I feel I can do it!",count:10},
+      ],
+      newPostText : "It-kamasutra",
+    }, 
+    dialogsPage  : {
+      dialogs: [
+        {id: 1,name: "Eduard"},
+        {id: 2,name: "Armen"},
+        {id: 3,name: "Vardan"},
+        {id: 4,name: "Karen"},
+        {id: 5,name: "Sargis"},
+      ],
+      
+      messages: [
+        {id: 1,message: "Hello!"},
+        {id: 2,message: "How are you?"},
+        {id: 3,message: "What is your name?"},
+      ],
+      newPostMessage :"",
+    },
+    singersPage : {
+       singers: [
+          {id:'1',singer:'Michael Jackson'},
+          {id:'2',singer:'Paul McCartney'},
+          {id:'3',singer:'Stevie Wonder'}
+        ],
+        songs: [
+          {id:"1",song:<a href="https://youtu.be/s3wNuru4U0I?t=2" target="_blank">We are the world</a>},
+          {id:"2",song:<a href="https://youtu.be/6d5ST3tbPIU" target="_blank">Let it be</a>},
+          {id:"3",song:<a href="https://youtu.be/1bGOgY1CmiU" target="_blank">I just called to say I love you</a>},
+        ],
+        newSingerName : "",
+        
+    }
+       
+        
   },
-  singersPage : {
-     singers: [
-        {id:'1',singer:'Michael Jackson'},
-        {id:'2',singer:'Paul McCartney'},
-        {id:'3',singer:'Stevie Wonder'}
-      ],
-      songs: [
-        {id:"1",song:<a href="https://youtu.be/s3wNuru4U0I?t=2" target="_blank">We are the world</a>},
-        {id:"2",song:<a href="https://youtu.be/6d5ST3tbPIU" target="_blank">Let it be</a>},
-        {id:"3",song:<a href="https://youtu.be/1bGOgY1CmiU" target="_blank">I just called to say I love you</a>},
-      ],
-      newSingerName : "",
-      
-  }
-     
-      
-}
-
-export let addPost = () => {
-  let newPost = {
-    id:5,
-    message: state.profilePage.newPostText,
-    count:12,
-  };
-     state.profilePage.posts.push(newPost);
-     state.profilePage.newPostText = "";
-     rerenderEntireTree(state);
-}
-
-export let updateNewText = (newText) => {
-  state.profilePage.newPostText = newText;
-     rerenderEntireTree(state);
-}
-
-export let addMessage = () => {
-  let newMessage = {
-    id:6,
-    name:state.dialogsPage.newPostMessage,
-  }
-  state.dialogsPage.dialogs.push(newMessage);
-  state.dialogsPage.newPostMessage = "";
-  rerenderEntireTree(state);
-}
-
-export let updateNewMessage = (newMessage) => {
+   addPost ()  {
+    let newPost = {
+      id:5,
+      message: state.profilePage.newPostText,
+      count:12,
+    };
+       state.profilePage.posts.push(newPost);
+       state.profilePage.newPostText = "";
+       rerenderEntireTree(state);
+  },
+  updateNewText (newText)  {
+    state.profilePage.newPostText = newText;
+       rerenderEntireTree(state);
+  },
+  addMessage () {
+    let newMessage = {
+      id:6,
+      name:state.dialogsPage.newPostMessage,
+    }
+    state.dialogsPage.dialogs.push(newMessage);
+    state.dialogsPage.newPostMessage = "";
+    rerenderEntireTree(state);
+  },
+  
+ updateNewMessage (newMessage)  {
   state.dialogsPage.newPostMessage = newMessage;
   rerenderEntireTree(state);
-}
-export let addSinger = () => {
+},
+ addSinger ()  {
   let newSinger = {
     id : 4,
     singer : state.singersPage.newSingerName,
@@ -85,11 +79,20 @@ export let addSinger = () => {
   state.singersPage.singers.push(newSinger);
   state.singersPage.newSingerName = "";
   rerenderEntireTree(state);
-}
+},
 
-export let updateNewSingerName = (newName) => {
+updateNewSingerName (newName)  {
   state.singersPage.newSingerName = newName;
   rerenderEntireTree();
+},
+
+subscribe (observer)  {
+  rerenderEntireTree = observer;
+},
+rerenderEntireTree ()  {
+  console.log('state');
+},
 }
 
-export default state;
+window store = store;
+export default store;
